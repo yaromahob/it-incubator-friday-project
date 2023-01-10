b5c9c6588b61eb25d13fc60ee1ef01303ab6db13
Bakhram <abdurakhimov93@list.ru>
2023-01-10 18:57:06 +0300

    profile page

---
+++ src/features/Profile/User/User.tsx
@@ -0,0 +1,50 @@
+import React, {ChangeEvent, useState} from 'react'
+import style from './User.module.scss'
+import edit from '../../../assets/svg/profile/edit.svg'
+import SuperButton from "../../../common/SuperButton/SuperButton";
+import {useAppDispatch, useAppSelector} from "../../../redux-store/store";
+import { changeUsersDataTC} from "../profileReducer";
+
+
+
+export const User = () => {
+    const nickName = useAppSelector(state => state.profile.name)
+    const appDispatch = useAppDispatch()
+    const [isOpen, setIsOpen] = useState(false)
+const [name,setName] = useState(nickName)
+    const openInput = () => {
+        setIsOpen(true)
+    }
+    const setUsersData = ()=>{
+        setIsOpen(false)
+        appDispatch(changeUsersDataTC(name,""))
+    }
+
+const changeNameHandler = (event:ChangeEvent<HTMLInputElement>)=>{
+
+    setName(event.currentTarget.value)
+}
+    return (
+        <div className={style.nickName}>
+
+            {isOpen
+                ?
+                <div className={style.input}>
+                    <div className={style.nick}>Nickname</div>
+                    <div className={style.inputContainer}>
+                        <input type="text" value={name} onChange={changeNameHandler} onBlur={setUsersData}/>
+                        <SuperButton onClick={setUsersData} className={style.saveButton}>
+                            SAVE
+                        </SuperButton>
+                    </div>
+                    <div className={style.border}></div>
+                </div>
+                :
+                <div>
+                    <span>{nickName}</span>
+                    <button onClick={openInput}><img src={edit}/></button>
+                </div>
+            }
+        </div>
+    )
+}
--- src/redux-store/store.ts
+++ src/redux-store/store.ts
@@ -1,21 +1,30 @@
 import {TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import {combineReducers, legacy_createStore } from 'redux'
-import {ThunkAction, ThunkDispatch } from 'redux-thunk'
-
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
+import {ThunkDispatch } from 'redux-thunk'
+import thunk from 'redux-thunk'
 import { authReducer } from './reducers/authReducer'
+import {profileReducer} from "../features/Profile/profileReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  profile:profileReducer,
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
-// создаем тип диспатча который принимает как AC так и TC
-export const useAppDispatch = ()=> useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
-// TodolistActionType | TaskActionType | loginActionType |appActionType;
-export type AppActionType = any
 
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AppActionType>
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionType>
+export const useAppDispatch =()=> useDispatch() as ThunkDispatch<AppRootStateType, unknown, AnyAction>
+// const appDispatch = useAppDispatch() //todo
+ export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+
+// // создаем тип диспатча который принимает как AC так и TC
+// export const useAppDispatch = ()=> useDispatch<AppDispatch>()
+
+//
+// // TodolistActionType | TaskActionType | loginActionType |appActionType;
+// export type AppActionType = any
+//
+// export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AppActionType>
+// export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionType>
