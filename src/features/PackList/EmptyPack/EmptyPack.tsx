52a9dcfa3acdb4b86990e45225a9f4c8177c178e
Bakhram <bakhram7493@gmail.com>
2023-01-20 16:30:22 +0300

    feat:searching card by text

--- src/features/PackList/EmptyPack/EmptyPack.tsx
+++ src/features/PackList/EmptyPack/EmptyPack.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import styles from '../PackList.module.scss'
+import styles from './EmptyPack.module.scss'
 import SuperButton from '../../../common/SuperButton/SuperButton'
 import { addPackTC } from '../PackList-reducer'
 import { useAppDispatch } from '../../../App/store'
+import { BackToPackList } from '../../BackToPackList/BackToPackList'
 
 export const EmptyPack = () => {
   const dispatch = useAppDispatch()
@@ -11,8 +12,13 @@ export const EmptyPack = () => {
   }
   return (
     <div>
-      <div className={styles.folder}>
+      <div className={styles.pack}>
+        <BackToPackList />
         <h2>Name Pack</h2>
+      </div>
+
+      <div className={styles.main}>
+        <div>This pack is empty. Click add new card to fill this pack</div>
         <SuperButton onClick={addPack}>Add new pack</SuperButton>
       </div>
     </div>
